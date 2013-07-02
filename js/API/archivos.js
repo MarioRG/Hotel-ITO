7a9b62a0296aaf816f17c21aec895f48855c1b65
MarioRG <mario.rios.guzman@gmail.com>
2013-07-02 17:53:35 -0500

    VERSION 2.0.C

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -1,30 +1,36 @@
 // archivos
 
-function subirFoto(foto, nom) {
+function subirFoto(foto, nom)
+{
 	var options = new FileUploadOptions();
 	options.fileKey="archivo";
-	options.fileName="Ivan.jpg";
+	options.fileName="Mario.jpg";
 	
 	var ft = new FileTransfer();
+	
 	ft.upload(foto, "http://www.igitsoft.com/pgtest.php", 
-	function(r){
+	function(r)
+	{
 		navigator.notification.confirm("Los datos han sido registrados satisfactoriamente", 
-		function(btn) {
-			switch(btn) {
+		function(btn)
+		{
+			switch(btn)
+			{
 				case 1:
-				navigator.notification.vibrate(5000);
-				break;
+					navigator.notification.vibrate(5000);
+					break;
 				case 2:
-				navigator.notification.beep(3);
-				break;
+					navigator.notification.beep(3);
+					break;
 				case 3:
-				navigator.notification.alert(infoDisp()['nombre'], null, "info", "Sí");
+					navigator.notification.alert(infoDisp()['nombre'], null, "info", "Sí");
 			}
 			registro(nom);
 			window.location.href="#main";
 		}, "Registro", "Vibrar,Sonar,Info,Cancelar");
 	}, 
-	function(err){
+	function(err)
+	{
 		navigator.notification.alert("Error al subir el archivo: "+errr.code, null, "Subir Registro", "Aceptar");
 	}, 
 	options);
